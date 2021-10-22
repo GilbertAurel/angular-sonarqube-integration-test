c668c814e08c2140ac9a70aaed6fae869252f4b8
Gilbert Aurel <aurel.gilbert10@gmail.com>
2021-10-22 14:13:23 +0700

    initalize project structure

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,10 +15,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'angular-sonarqube-integration-test'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-sonarqube-integration-test');
-  });
 });
